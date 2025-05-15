f782597256007ade88a5577c87151e3ff340e87c
abhiyansh <abhiyansharya161@gmail.com>
2025-05-15 14:04:00 +0530

    Fix: Backend env port

--- shared/config.js
+++ shared/config.js
@@ -1,3 +1,3 @@
-export const SERVER_PORT = process.env.PORT || 3000;
+export const SERVER_PORT = process.env.SERVER_PORT || 3000;
 export const CLIENT_BASE_URL = process.env.CLIENT_BASE_URL || 'http://localhost:5173';
 export const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'http://localhost:3000';
